02a00689161a94c48404d9894b94eb57874fc491
Istiyak <istiyak.riyad@gmail.com>
2024-04-05 21:46:56 +0600

    Adding test file restaurant

---
+++ transport/http/restaurant_test.go
@@ -0,0 +1,116 @@
+package http_test
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+	"gitlab.com/IstiyakRiyad/technical-assessment-pathao/db"
+	"gitlab.com/IstiyakRiyad/technical-assessment-pathao/internal/restaurant"
+	transportHttp "gitlab.com/IstiyakRiyad/technical-assessment-pathao/transport/http"
+)
+
+
+func init() {
+	viper.SetConfigType("env")
+	viper.SetConfigName("dev")
+	viper.AddConfigPath("../..")
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+}
+
+
+func TestGetRestaurants(t *testing.T) {
+	db, _:= db.NewDatabase()
+
+	service := restaurant.NewRestaurantService(db)
+	trasport := transportHttp.NewHandler(service)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/api/v1/restaurant/", nil)
+
+	trasport.Router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+}
+
+
+func TestGetRestaurantByDateTime(t *testing.T) {
+	db, _ := db.NewDatabase()
+	service := restaurant.NewRestaurantService(db)
+	trasport := transportHttp.NewHandler(service)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/api/v1/restaurant/datetime", nil)
+
+	trasport.Router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+}
+
+
+func TestGetRestaurantById(t *testing.T) {
+	db, _ := db.NewDatabase()
+	service := restaurant.NewRestaurantService(db)
+	trasport := transportHttp.NewHandler(service)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/api/v1/restaurant/12", nil)
+
+	trasport.Router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+}
+
+func TestSearch(t *testing.T) {
+	db, _ := db.NewDatabase()
+	service := restaurant.NewRestaurantService(db)
+	trasport := transportHttp.NewHandler(service)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/api/v1/search/?search_query=beef", nil)
+
+	trasport.Router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+}
+
+func TestGetUsers(t *testing.T) {
+	db, _ := db.NewDatabase()
+	service := restaurant.NewRestaurantService(db)
+	trasport := transportHttp.NewHandler(service)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/api/v1/user/", nil)
+
+	trasport.Router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+}
+
+func TestGetUserById(t *testing.T) {
+	db, _ := db.NewDatabase()
+	service := restaurant.NewRestaurantService(db)
+	trasport := transportHttp.NewHandler(service)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/api/v1/user/3", nil)
+
+	trasport.Router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+}
+
+
+
